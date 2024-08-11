f6711de7bcd7c767ca98106d9f445b36bc8cefd2
Martin Høst Normark <martin.hoest.normark@lego.com>
2024-08-11 12:06:27 +0200

    chore: Refactor chamfer_distance function and update documentation

--- metrics/chamfer_distance.py
+++ metrics/chamfer_distance.py
@@ -2,7 +2,20 @@ import trimesh
 import numpy as np
 
 
-def chamfer_distance(mesh1, mesh2, samples=10000):
+def chamfer_distance(
+    mesh1: trimesh.Trimesh, mesh2: trimesh.Trimesh, samples: int = 10000
+):
+    """
+    Calculate the Chamfer distance between two meshes.
+
+    Parameters:
+    mesh1 (trimesh.Trimesh): The first input mesh.
+    mesh2 (trimesh.Trimesh): The second input mesh.
+    samples (int): The number of samples to use for the calculation.
+
+    Returns:
+    float: The Chamfer distance metric
+    """
     points1 = mesh1.sample(samples)
     points2 = mesh2.sample(samples)
 
@@ -10,4 +23,5 @@ def chamfer_distance(mesh1, mesh2, samples=10000):
     _, distances2, _ = trimesh.proximity.closest_point(mesh1, points2)
 
     chamfer_dist = np.mean(distances1) + np.mean(distances2)
+
     return chamfer_dist
