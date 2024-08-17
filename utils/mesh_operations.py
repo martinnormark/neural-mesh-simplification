ed3c97c9b7181e2a765d7134169c93322816b33b
Martin Høst Normark <martin.hoest.normark@lego.com>
2024-08-17 12:04:20 +0200

    chore: Add build_graph_from_mesh function to mesh_operations module

--- utils/mesh_operations.py
+++ utils/mesh_operations.py
@@ -1,3 +1,7 @@
+import networkx as nx
+import trimesh
+
+
 def simplify_mesh(mesh, target_faces):
     # Simplify a mesh to a target number of faces
     pass
@@ -11,3 +15,25 @@ def calculate_mesh_features(mesh):
 def align_meshes(mesh1, mesh2):
     # Align two meshes (useful for comparison)
     pass
+
+
+def compare_meshes(mesh1, mesh2):
+    # Compare two meshes (e.g., Hausdorff distance)
+    pass
+
+
+def build_graph_from_mesh(mesh: trimesh.Trimesh) -> nx.Graph:
+    """Build a graph structure from a mesh."""
+    G = nx.Graph()
+
+    # Add nodes (vertices)
+    for i, vertex in enumerate(mesh.vertices):
+        G.add_node(i, pos=vertex)
+
+    # Add edges
+    for face in mesh.faces:
+        G.add_edge(face[0], face[1])
+        G.add_edge(face[1], face[2])
+        G.add_edge(face[2], face[0])
+
+    return G
